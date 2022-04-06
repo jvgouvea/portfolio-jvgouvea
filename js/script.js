6ea87149799c904e1668c68316e7d41b37c4b34b
jvgouvea <contato.jvgouvea@gmail.com>
2022-04-06 16:58:36 -0300

    fit scroll reveal button gotop

--- js/script.js
+++ js/script.js
@@ -45,4 +45,21 @@ function initTooltip() {
   }
 }
 
-initTooltip()
+initTooltip();
+
+//Scroll reveal no botao gotop
+function initMostrarGotop() {
+  function mostrarGotop() {
+    const btntop = document.querySelector(".gotop");
+    const distanciaScroll = window.innerHeight * 0.6;
+
+    if (window.scrollY > distanciaScroll) {
+      btntop.classList.add("ativo");
+    } else {
+      btntop.classList.remove("ativo");
+    }
+  }
+
+  window.addEventListener("scroll", mostrarGotop);
+}
+initMostrarGotop();
